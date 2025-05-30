2b33b0e49ebf62cb817ef8ca67e4127601d672c5
Adriano Lima <jink_adriano@hotmail.com>
2025-05-30 20:45:28 -0300

    projeto aula - 26-05

--- app/src/Header2.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Header2({children}){
-    return (
-        <h1>{children}</h1>
-    );
-}
---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React, {useState}from 'react';
+import Login from './pages/Login';
+import './global.css';
+
+export default function App() {
+
+  return (
+    <Login />
+  );
+}
