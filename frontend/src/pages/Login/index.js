54d3642a661cbe394b6805f990b90a878e674abb
Adriano Lima <jink_adriano@hotmail.com>
2025-05-30 21:09:22 -0300

    aula 30-05

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import logoImage from '../../assets/logo.png';
+import padLock from '../../assets/padlock.png';
+
 export default function Login() {
 
   return (
-    <div>
-  
+    <div className="login-container">
+      <section className="form">
+          <img 
+
+
+      </section>
     </div>
   );
 }
